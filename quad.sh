ad16ae42e04a5cabadc14d8b56fa2ca8f2a541f7
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-17 15:01:54 +0530

    quadratic equation

---
+++ quad.sh
@@ -0,0 +1,23 @@
+echo "enter value for a: "
+read a
+echo "enter value for b: "
+read b
+echo "enter value for c: "
+read c
+root1=0
+root2=0
+delta=$(awk -v x=$a -v y=$b -v z=$c 'BEGIN { print (y*y-4*x*z ) } ')
+if (( $(awk -v var=$delta 'BEGIN { print ( delta > 0 ) } ') ))
+then
+root1=$( awk -v det=$delta -v x=$a -v y=$b 'BEGIN {print ((-y + (det**0.5))/(2*x)) }')
+root2=$( awk -v det=$delta -v x=$a -v y=$b 'BEGIN {print ((-y - (det**0.5))/(2*x)) }'  )
+echo root1 and root2 are $root1 , $root2
+elif (( $delta == 0 ))
+then
+root1=$(awk -v x=$a -v y=$b 'BEGIN {print(-y/(2*x)) } ')
+echo $root1
+else
+actual=$(awk -v x=$a -v y=$b 'BEGIN { print (-y/(2*x) ) } '  )
+imaginary=$(awk -v det=$delta -v x=$a 'BEGIN{print ((-det/(2*x))**0.5)}')
+echo "( $actual ,  $imaginary )"
+fi
