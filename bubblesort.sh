f5c50b7737ef159fe3285830f61e6f2c71e39d86
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:35:42 +0530

    [Vishnu V Dev]Refactor.Added comments

--- bubblesort.sh
+++ bubblesort.sh
@@ -1,30 +1,30 @@
 #!/bin/bash -x
 echo "enter size "
- read n
-echo "enter the elements of array"
-for (( i=0; i< $n;i++))
+read n
+echo "enter the elements of array" 
+for (( i=0; i< $n;i++)) #loop to enter the elements of array
 do
-  read nos[$i]
+  read nos[$i] #reading the elements of array
   echo " array in orginal order "
-  for (( i=0; i<$n; i++ ))
+  for (( i=0; i<$n; i++ )) #loop to display the array
   do
     echo ${nos[$i]}
   done
-for ((i = 0; i<n;i++))
+for ((i = 0; i<n;i++)) #sorting of array 
 do
   for (( j=1; j<n-i-1;j++))
   do
-    if ((${nos[$i]} > ${nos[$((j+1))]}))
+    if ((${nos[$i]} > ${nos[$((j+1))]})) #checking for the condition
     then
-    temp = ${nos[$j]}
+    temp = ${nos[$j]} #swapping 
     arr[$j] = ${nos[$((j+1))]}  
     arr[$((j+1))] = $temp
     fi
    done
 done
 echo "sorted"
-for (( i =0; i< $n;i++))
+for (( i =0; i< $n;i++)) #loop to display the sorted array
 do
-  echo" ${nos[$i]}"
+  echo" ${nos[$i]}" #display of the sorted array
 done
 
