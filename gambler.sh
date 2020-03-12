41013ce79ce7a7cb66dc83799bb7be674ef40f64
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:53:25 +0530

    [Vishnu]Refactor.Added comments

--- gambler.sh
+++ gambler.sh
@@ -1,4 +1,5 @@
-echo "enter the stake "
+#!/bin/bash -x
+echo "enter the stake " #input the stake goal and number of times
 read stake
 echo "enter  your goal"
 read goal
@@ -6,7 +7,7 @@ echo "Number of times!!"
 read n
 win=0
 loss=0
-while (($stake>0))&&(($goal!=$stake))&&(($n>0))
+while (($stake>0))&&(($goal!=$stake))&&(($n>0)) #while loop to check the conditions to move forward to the program
 do
   echo "enter the bet amount"
   read bet
@@ -15,20 +16,20 @@ do
     echo "bet amount is greater than stack"
     continue
   fi
-  r=0.$(($RANDOM % 10 ));
-  if (($(awk -v x=$r -v y=0.5 'BEGIN {print(x>y)}')))
+  r=0.$(($RANDOM % 10 )); #generating random number
+  if (($(awk -v x=$r -v y=0.5 'BEGIN {print(x>y)}'))) #checking condition for win or loss
   then
-    echo "you won the bet"
+    echo "you won the bet" #condition for win 
     stake=$((stake + bet))
     win=$((win + 1))
-  else
+  else                      #condition for false
     echo "you lose the bet"
     stake=$((stake - bet))
     loss=$((loss + 1))
   fi
   echo "stake is : $stake"
 done
-echo "number of wins is $win"
+echo "number of wins is $win" #printing of final results
 echo "number of loss is $loss"
 winp=$(( win /( win + loss )))
 lossp=$(( loss/( win + loss )))
