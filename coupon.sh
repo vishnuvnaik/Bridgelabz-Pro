22177b47e73776f271c4afe2b6118954138a2f2f
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:38:15 +0530

    [Vishnu]Refactor.indentation done

--- coupon.sh
+++ coupon.sh
@@ -1,14 +1,15 @@
+#!/bin/bash -x
 # generating a coupon randomly
 #setting the arrays for coupon number
 read -p 'how many distinct coupon you want to be generated....: ' n
 while [ $n -gt 0 ]
-do
-a=$(shuf -i 0-25 -n 1)
-b=$(shuf -i 0-25 -n 1)
-random1=$( shuf -i 0-25 -n 1 )
-random2=$( shuf -i 0-25 -n 1 )
-random3=$( shuf -i 100-1000 -n 1 )
-random4=$( shuf -i 1000-2000 -n 1 )
+ do
+    a=$(shuf -i 0-25 -n 1)
+    b=$(shuf -i 0-25 -n 1)
+    random1=$( shuf -i 0-25 -n 1 )
+    random2=$( shuf -i 0-25 -n 1 )
+    random3=$( shuf -i 100-1000 -n 1 )
+    random4=$( shuf -i 1000-2000 -n 1 )
 #coupon number generated with 6 random number generations
 echo -ne "\r the generated coupon number is:"
 echo $random4${small[$random1]}$random4${big[$random2]}$random3${small[$a]}
