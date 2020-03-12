b5974e3e1cd700d2a897448039681c6b2c38355c
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:47:28 +0530

    [Vishnu]Refactor.Added comments

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 echo "welcome"
-echo  "Enter = the no of time you want to flip coin : "
+echo  "Enter = the no of time you want to flip coin : " 
 read n
 heads=0
 tails=0
-for((i=0;i<=n;i++))
+for((i=0;i<=n;i++)) #loop to generate random numbers and checking the conditons for flipcoin
 do
-  randomnum=$( shuf -i 0-10 -n 1 )
+  randomnum=$( shuf -i 0-10 -n 1 ) #random number generation
   if (( randomnum < 5 ))
   then
     let tails++
@@ -14,7 +14,7 @@ do
     let heads++
   fi
 done
-echo "heads= $heads"
+echo "heads= $heads" #printing of final result
 echo "tails= $tails"
 hp=$(awk -v a=$heads -v b=$n ' BEGIN { print(a/b *100 ) } ')
 tp=$(awk -v a=$tails -v b=$n ' BEGIN { print(a/b *100 ) } ')
