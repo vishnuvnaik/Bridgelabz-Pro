98665bef6200242c3ad8e5f054655bfe87b9adaa
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:55:05 +0530

    [Vishnu]Refactor.Added comments

--- harmonic.sh
+++ harmonic.sh
@@ -1,9 +1,9 @@
-echo Enter a number
+#!/bin/bash -x
+echo Enter a number #input the value of n
 read n
 sum=0
 for (( i=1;i<=n;i++ ))
 do
-  sum=$( awk -v a=$sum -v b=$i 'BEGIN { print ( a + 1/b ) } ')
-
+  sum=$( awk -v a=$sum -v b=$i 'BEGIN { print ( a + 1/b ) } ') #harmonic series computation
 done
-echo "harmonic number is $sum"
+echo "harmonic number is $sum" #display
