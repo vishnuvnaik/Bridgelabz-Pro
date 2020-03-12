e412d57f88679a621cea445ea139173116d08357
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:07:21 +0530

    [Vishnu V Dev]Refactor.modified anagram.sh

--- anagram.sh
+++ anagram.sh
@@ -1,9 +1,10 @@
+#!/bin/bash -x
 prep()
 {
-fold -w! <<< "${1//[^[:alnum:]]/}" | tr '[:upper:]' '[:lower:]' | sort | tr -d '\n'
+  fold -w! <<< "${1//[^[:alnum:]]/}" | tr '[:upper:]' '[:lower:]' | sort | tr -d '\n'
 }
 isAnagram(){
-a = $(prep "$1")
-b = $(prep "$2")
+  a = $(prep "$1")
+  b = $(prep "$2")
 [[$a == $b]] && echo "yes" || echo "no"
 }
