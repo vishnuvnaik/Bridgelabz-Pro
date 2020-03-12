9451e92f49e9b917549fa08e1c6df355fc6cd989
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 13:13:45 +0530

    [Vishnu]Refactor.Added comments

--- TemperatureConversion.sh
+++ TemperatureConversion.sh
@@ -1,24 +1,23 @@
 #!/bin/bash -x
-echo "*** Converting between the different temperature scales ***"
+echo "*** Converting between the different temperature scales ***" #inputting the choice ctof to ftoc or vice versa  
 echo "1. Convert Celsius temperature into Fahrenheit"
 echo "2. Convert Fahrenheit temperatures into Celsius"
 echo -n "Select your choice (1-2) : "
 read choice
- 
-if [ $choice -eq 1 ]
+  
+if [ $choice -eq 1 ]   #condition for choice 1 c to f 
 then
  echo -n "Enter temperature (C) : "
  read tc
  tf=$(echo "scale=2;((9/5) * $tc) + 32" |bc)
  echo "$tc C = $tf F"
-elif [ $choice -eq 2 ]
+elif [ $choice -eq 2 ] #condition for choice 2 f to c 
 then
  echo -n "Enter temperature (F) : "
  read tf
-
  tc=$(echo "scale=2;(5/9)*($tf-32)"|bc)
  echo "$tf = $tc"
-else
+else              #invalid
  echo "Please select 1 or 2 only"
  exit 1
 fi
