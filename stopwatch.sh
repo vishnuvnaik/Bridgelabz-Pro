642764b67f51aa3f379f81efe90a72db5f11c614
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 13:07:58 +0530

    [Vishnu]Refactor.Added comments

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,9 +1,10 @@
+#!/bin/bash -x
 echo "Enter any key to start "
 read n
-start=$( date +%s)
+start=$( date +%s) #taking unix time using %s and starting the stopwatch
 echo "start time is $start"
 echo "enter any key stop stopwatch"
 read e
-stop=$( date +%s)
+stop=$( date +%s) #again taking unix time and stopping the stopwatch 
 elapsed=$(($stop - $start))
 echo "elapsed time is $elapsed"
