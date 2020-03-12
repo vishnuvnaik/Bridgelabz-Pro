e4280e421e568f9a21fea1ff98c19d873c31f254
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-03-12 12:19:31 +0530

    [Vishnu V Dev]Refactor.Added Comments

--- anagram.sh
+++ anagram.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
-prep()
+prep() #function for some purposes listed below
 {
-  fold -w! <<< "${1//[^[:alnum:]]/}" | tr '[:upper:]' '[:lower:]' | sort | tr -d '\n'
+  fold -w! <<< "${1//[^[:alnum:]]/}" | tr '[:upper:]' '[:lower:]' | sort | tr -d '\n' 
+                                                                                # 1. Remove all alphanumerics
+                                                                                # 2. Convert to all lowercasing all characters 
+                                                                                # 3. Sorting all characters
+                                                                                # 4. Stripping all newlines 
 }
-isAnagram(){
+isAnagram(){ #function to check if given 2 arguments are anagrams
   a = $(prep "$1")
   b = $(prep "$2")
 [[$a == $b]] && echo "yes" || echo "no"
